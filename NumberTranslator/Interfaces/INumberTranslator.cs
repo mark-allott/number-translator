64be88698c44e4e62435df9cfe8312fe4abd98f4
Mark Allott <github.mark-allott@malsoft.com>
2025-06-16 12:23:33 +0100

    Add interface to expand ITranslator to allow for decimal, currency and min/max values for conversion of numbers to words

---
+++ NumberTranslator/Interfaces/INumberTranslator.cs
@@ -0,0 +1,25 @@
+﻿namespace NumberTranslator.Interfaces;
+
+public interface INumberTranslator
+	: ITranslator
+{
+	/// <summary>
+	/// Flag to indicate whether to allow fractional parts of numbers in translation
+	/// </summary>
+	bool AllowDecimals { get; }
+
+	/// <summary>
+	/// Flag to indicate whether to translate using currency
+	/// </summary>
+	bool AllowCurrency { get; }
+
+	/// <summary>
+	/// Defines what the minimum value of the input is
+	/// </summary>
+	double MinimumValue { get; }
+
+	/// <summary>
+	/// Defines the maximum permitted value for the input
+	/// </summary>
+	double MaximumValue { get; }
+}
