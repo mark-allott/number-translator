0d3badaacb8ba5b32e69177bc3ab9355e4ca7847
Mark Allott <github.mark-allott@malsoft.com>
2025-06-18 12:28:20 +0100

    Add naming strategy classes for default, British and American

---
+++ NumberTranslator/Strategies/BritishCurrencyNamingStrategy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NumberTranslator.Interfaces;
+
+namespace NumberTranslator.Strategies;
+
+public class BritishCurrencyNamingStrategy
+	: ICurrencyNamingStrategy, IAutoRegister
+{
+	#region ICurrencyNamingStrategy implementation
+
+	/// <inheritdoc />
+	public string CurrencyIntegralPartName(long value)
+	{
+		return Math.Abs(value) switch
+		{
+			1 => "pound",
+			_ => "pounds"
+		};
+	}
+
+	/// <inheritdoc />
+	public string CurrencyFractionalPartName(long value)
+	{
+		return Math.Abs(value) switch
+		{
+			1 => "penny",
+			_ => "pence"
+		};
+	}
+
+	#endregion ICurrencyNamingStrategy implementation
+}
