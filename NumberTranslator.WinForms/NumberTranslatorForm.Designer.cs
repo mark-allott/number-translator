c743218e4a3d69602391e63810a126dd4a79c54f
Mark Allott <github.mark-allott@malsoft.com>
2025-06-21 13:13:37 +0100

    Perform simple translations

--- NumberTranslator.WinForms/NumberTranslatorForm.Designer.cs
+++ NumberTranslator.WinForms/NumberTranslatorForm.Designer.cs
@@ -30,8 +30,10 @@
 		{
 			splitContainer1 = new SplitContainer();
 			translationTextBox = new TextBox();
-			valueTextBox = new TextBox();
+			capitaliseNumbersCheckBox = new CheckBox();
+			clearTranslationsButton = new Button();
 			translateButton = new Button();
+			valueTextBox = new TextBox();
 			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
 			splitContainer1.Panel1.SuspendLayout();
 			splitContainer1.Panel2.SuspendLayout();
@@ -44,6 +46,7 @@
 			splitContainer1.FixedPanel = FixedPanel.Panel2;
 			splitContainer1.IsSplitterFixed = true;
 			splitContainer1.Location = new Point(0, 0);
+			splitContainer1.Margin = new Padding(4);
 			splitContainer1.Name = "splitContainer1";
 			splitContainer1.Orientation = Orientation.Horizontal;
 			// 
@@ -54,50 +57,85 @@
 			// 
 			// splitContainer1.Panel2
 			// 
+			splitContainer1.Panel2.Controls.Add(capitaliseNumbersCheckBox);
+			splitContainer1.Panel2.Controls.Add(clearTranslationsButton);
 			splitContainer1.Panel2.Controls.Add(translateButton);
 			splitContainer1.Panel2.Controls.Add(valueTextBox);
-			splitContainer1.Panel2.Margin = new Padding(4);
-			splitContainer1.Size = new Size(784, 561);
-			splitContainer1.SplitterDistance = 500;
+			splitContainer1.Panel2.Margin = new Padding(5, 6, 5, 6);
+			splitContainer1.Size = new Size(1008, 785);
+			splitContainer1.SplitterDistance = 693;
+			splitContainer1.SplitterWidth = 6;
 			splitContainer1.TabIndex = 1;
 			// 
 			// translationTextBox
 			// 
 			translationTextBox.Dock = DockStyle.Fill;
 			translationTextBox.Location = new Point(0, 0);
+			translationTextBox.Margin = new Padding(4);
 			translationTextBox.Multiline = true;
 			translationTextBox.Name = "translationTextBox";
 			translationTextBox.ReadOnly = true;
-			translationTextBox.Size = new Size(784, 500);
+			translationTextBox.ScrollBars = ScrollBars.Both;
+			translationTextBox.Size = new Size(1008, 693);
 			translationTextBox.TabIndex = 0;
 			// 
-			// valueTextBox
+			// capitaliseNumbersCheckBox
 			// 
-			valueTextBox.Location = new Point(8, 16);
-			valueTextBox.Name = "valueTextBox";
-			valueTextBox.PlaceholderText = "Enter value to translate";
-			valueTextBox.Size = new Size(256, 23);
-			valueTextBox.TabIndex = 0;
+			capitaliseNumbersCheckBox.AutoSize = true;
+			capitaliseNumbersCheckBox.Checked = true;
+			capitaliseNumbersCheckBox.CheckState = CheckState.Checked;
+			capitaliseNumbersCheckBox.Location = new Point(601, 11);
+			capitaliseNumbersCheckBox.Name = "capitaliseNumbersCheckBox";
+			capitaliseNumbersCheckBox.Size = new Size(96, 25);
+			capitaliseNumbersCheckBox.TabIndex = 3;
+			capitaliseNumbersCheckBox.Text = "Capitalise";
+			capitaliseNumbersCheckBox.UseVisualStyleBackColor = true;
+			capitaliseNumbersCheckBox.CheckedChanged += capitliseNumbersCheckBox_CheckedChanged;
+			// 
+			// clearTranslationsButton
+			// 
+			clearTranslationsButton.Location = new Point(489, 11);
+			clearTranslationsButton.Margin = new Padding(4);
+			clearTranslationsButton.Name = "clearTranslationsButton";
+			clearTranslationsButton.Size = new Size(93, 50);
+			clearTranslationsButton.TabIndex = 2;
+			clearTranslationsButton.Text = "Clear";
+			clearTranslationsButton.UseVisualStyleBackColor = true;
+			clearTranslationsButton.Click += clearTranslationsButton_Click;
 			// 
 			// translateButton
 			// 
-			translateButton.Location = new Point(278, 9);
+			translateButton.Location = new Point(357, 11);
+			translateButton.Margin = new Padding(4);
 			translateButton.Name = "translateButton";
-			translateButton.Size = new Size(96, 36);
+			translateButton.Size = new Size(123, 50);
 			translateButton.TabIndex = 1;
 			translateButton.Text = "Translate";
 			translateButton.UseVisualStyleBackColor = true;
+			translateButton.Click += translateButton_Click;
+			// 
+			// valueTextBox
+			// 
+			valueTextBox.Location = new Point(10, 22);
+			valueTextBox.Margin = new Padding(4);
+			valueTextBox.Name = "valueTextBox";
+			valueTextBox.PlaceholderText = "Enter value to translate";
+			valueTextBox.Size = new Size(328, 29);
+			valueTextBox.TabIndex = 0;
 			// 
 			// NumberTranslatorForm
 			// 
 			AcceptButton = translateButton;
-			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleDimensions = new SizeF(9F, 21F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(784, 561);
+			ClientSize = new Size(1008, 785);
 			Controls.Add(splitContainer1);
+			Font = new Font("Segoe UI", 12F);
+			Margin = new Padding(4);
 			MinimizeBox = false;
 			Name = "NumberTranslatorForm";
 			Text = "Number Translator";
+			Activated += NumberTranslatorForm_Activated;
 			splitContainer1.Panel1.ResumeLayout(false);
 			splitContainer1.Panel1.PerformLayout();
 			splitContainer1.Panel2.ResumeLayout(false);
@@ -113,5 +151,7 @@
 		private TextBox translationTextBox;
 		private Button translateButton;
 		private TextBox valueTextBox;
+		private Button clearTranslationsButton;
+		private CheckBox capitaliseNumbersCheckBox;
 	}
 }
--- NumberTranslator.WinForms/NumberTranslatorForm.cs
+++ NumberTranslator.WinForms/NumberTranslatorForm.cs
@@ -1,10 +1,62 @@
+using System.Globalization;
+
 namespace NumberTranslator.WinForms
 {
 	public partial class NumberTranslatorForm : Form
 	{
-		public NumberTranslatorForm()
+		private readonly NumberTranslatorFactory _factory;
+
+		public NumberTranslatorForm(NumberTranslatorFactory factory)
 		{
+			_factory = factory ?? throw new ArgumentNullException(nameof(factory));
 			InitializeComponent();
+		}
+
+		private void SetDefaultFocus()
+		{
+			valueTextBox.SelectAll();
+			valueTextBox.Focus();
+		}
+
+		private void translateButton_Click(object sender, EventArgs e)
+		{
+			var translator = _factory.GetNumberTranslator("en");
+			var input = valueTextBox.Text.Trim();
+			try
+			{
+				var response = translator.Translate(input);
+				if (capitaliseNumbersCheckBox.Checked)
+				{
+					var ti = new CultureInfo(translator.LanguageCodeId).TextInfo;
+					var parts = response.Split(' ', StringSplitOptions.TrimEntries)
+						.Select(s => !s.Equals("and") ? ti.ToTitleCase(s) : s)
+						.ToArray();
+					response = string.Join(' ', parts);
+				}
+				translationTextBox.AppendText($"'{input}' translates to {response}" + Environment.NewLine);
+			}
+			catch (Exception exception)
+			{
+				translationTextBox.AppendText($"'{input}' cannot be translated because '{exception.Message}'" + Environment.NewLine);
+			}
+			SetDefaultFocus();
+		}
+
+		private void NumberTranslatorForm_Activated(object sender, EventArgs e)
+		{
+			SetDefaultFocus();
+		}
+
+		private void clearTranslationsButton_Click(object sender, EventArgs e)
+		{
+			translationTextBox.Clear();
+			valueTextBox.Clear();
+			SetDefaultFocus();
+		}
+
+		private void capitliseNumbersCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			SetDefaultFocus();
 		}
 	}
 }
