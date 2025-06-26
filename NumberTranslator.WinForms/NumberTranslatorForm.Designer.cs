2017f0ff30a3f0d9a819c2726332fd2baa6b4bc3
Mark Allott <github.mark-allott@malsoft.com>
2025-06-26 09:00:03 +0100

    Update to include additional settings options such as currency, decimal places and alternate languages

--- NumberTranslator.WinForms/NumberTranslatorForm.Designer.cs
+++ NumberTranslator.WinForms/NumberTranslatorForm.Designer.cs
@@ -30,14 +30,31 @@
 		{
 			splitContainer1 = new SplitContainer();
 			translationTextBox = new TextBox();
-			capitaliseNumbersCheckBox = new CheckBox();
-			clearTranslationsButton = new Button();
-			translateButton = new Button();
+			splitContainer2 = new SplitContainer();
+			settingsToggleButton = new Button();
 			valueTextBox = new TextBox();
+			translateButton = new Button();
+			clearTranslationsButton = new Button();
+			capitaliseNumbersCheckBox = new CheckBox();
+			allowMultilingualCheckbox = new CheckBox();
+			languagesCombobox = new ComboBox();
+			maximumValueTextbox = new TextBox();
+			label3 = new Label();
+			minimumValueTextbox = new TextBox();
+			label2 = new Label();
+			label1 = new Label();
+			decimalPlacesUpDown = new NumericUpDown();
+			includeDecimalPlacesCheckbox = new CheckBox();
+			includeCurrencyCheckbox = new CheckBox();
 			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
 			splitContainer1.Panel1.SuspendLayout();
 			splitContainer1.Panel2.SuspendLayout();
 			splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+			splitContainer2.Panel1.SuspendLayout();
+			splitContainer2.Panel2.SuspendLayout();
+			splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)decimalPlacesUpDown).BeginInit();
 			SuspendLayout();
 			// 
 			// splitContainer1
@@ -57,14 +74,10 @@
 			// 
 			// splitContainer1.Panel2
 			// 
-			splitContainer1.Panel2.Controls.Add(capitaliseNumbersCheckBox);
-			splitContainer1.Panel2.Controls.Add(clearTranslationsButton);
-			splitContainer1.Panel2.Controls.Add(translateButton);
-			splitContainer1.Panel2.Controls.Add(valueTextBox);
-			splitContainer1.Panel2.Margin = new Padding(5, 6, 5, 6);
+			splitContainer1.Panel2.Controls.Add(splitContainer2);
+			splitContainer1.Panel2.Margin = new Padding(5);
 			splitContainer1.Size = new Size(1008, 785);
-			splitContainer1.SplitterDistance = 693;
-			splitContainer1.SplitterWidth = 6;
+			splitContainer1.SplitterDistance = 656;
 			splitContainer1.TabIndex = 1;
 			// 
 			// translationTextBox
@@ -76,56 +89,192 @@
 			translationTextBox.Name = "translationTextBox";
 			translationTextBox.ReadOnly = true;
 			translationTextBox.ScrollBars = ScrollBars.Both;
-			translationTextBox.Size = new Size(1008, 693);
+			translationTextBox.Size = new Size(1008, 656);
 			translationTextBox.TabIndex = 0;
+			// 
+			// splitContainer2
+			// 
+			splitContainer2.Dock = DockStyle.Fill;
+			splitContainer2.FixedPanel = FixedPanel.Panel1;
+			splitContainer2.Location = new Point(0, 0);
+			splitContainer2.Margin = new Padding(0);
+			splitContainer2.Name = "splitContainer2";
+			splitContainer2.Orientation = Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			splitContainer2.Panel1.Controls.Add(settingsToggleButton);
+			splitContainer2.Panel1.Controls.Add(valueTextBox);
+			splitContainer2.Panel1.Controls.Add(translateButton);
+			splitContainer2.Panel1.Controls.Add(clearTranslationsButton);
+			splitContainer2.Panel1.Controls.Add(capitaliseNumbersCheckBox);
+			// 
+			// splitContainer2.Panel2
+			// 
+			splitContainer2.Panel2.Controls.Add(allowMultilingualCheckbox);
+			splitContainer2.Panel2.Controls.Add(languagesCombobox);
+			splitContainer2.Panel2.Controls.Add(maximumValueTextbox);
+			splitContainer2.Panel2.Controls.Add(label3);
+			splitContainer2.Panel2.Controls.Add(minimumValueTextbox);
+			splitContainer2.Panel2.Controls.Add(label2);
+			splitContainer2.Panel2.Controls.Add(label1);
+			splitContainer2.Panel2.Controls.Add(decimalPlacesUpDown);
+			splitContainer2.Panel2.Controls.Add(includeDecimalPlacesCheckbox);
+			splitContainer2.Panel2.Controls.Add(includeCurrencyCheckbox);
+			splitContainer2.Size = new Size(1008, 125);
+			splitContainer2.SplitterDistance = 48;
+			splitContainer2.SplitterWidth = 1;
+			splitContainer2.TabIndex = 4;
+			splitContainer2.TabStop = false;
+			// 
+			// settingsToggleButton
+			// 
+			settingsToggleButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			settingsToggleButton.Location = new Point(764, 4);
+			settingsToggleButton.Margin = new Padding(4);
+			settingsToggleButton.Name = "settingsToggleButton";
+			settingsToggleButton.Size = new Size(240, 40);
+			settingsToggleButton.TabIndex = 8;
+			settingsToggleButton.Text = "Settings";
+			settingsToggleButton.UseVisualStyleBackColor = true;
+			settingsToggleButton.Click += settingsToggleButton_Click;
+			// 
+			// valueTextBox
+			// 
+			valueTextBox.Location = new Point(8, 12);
+			valueTextBox.Margin = new Padding(4);
+			valueTextBox.Name = "valueTextBox";
+			valueTextBox.PlaceholderText = "Enter value to translate";
+			valueTextBox.Size = new Size(328, 29);
+			valueTextBox.TabIndex = 7;
+			// 
+			// translateButton
+			// 
+			translateButton.Location = new Point(344, 4);
+			translateButton.Margin = new Padding(4);
+			translateButton.Name = "translateButton";
+			translateButton.Size = new Size(120, 40);
+			translateButton.TabIndex = 6;
+			translateButton.Text = "Translate";
+			translateButton.UseVisualStyleBackColor = true;
+			translateButton.Click += translateButton_Click;
+			// 
+			// clearTranslationsButton
+			// 
+			clearTranslationsButton.Location = new Point(472, 4);
+			clearTranslationsButton.Margin = new Padding(4);
+			clearTranslationsButton.Name = "clearTranslationsButton";
+			clearTranslationsButton.Size = new Size(120, 40);
+			clearTranslationsButton.TabIndex = 5;
+			clearTranslationsButton.Text = "Clear";
+			clearTranslationsButton.UseVisualStyleBackColor = true;
+			clearTranslationsButton.Click += clearTranslationsButton_Click;
 			// 
 			// capitaliseNumbersCheckBox
 			// 
 			capitaliseNumbersCheckBox.AutoSize = true;
 			capitaliseNumbersCheckBox.Checked = true;
 			capitaliseNumbersCheckBox.CheckState = CheckState.Checked;
-			capitaliseNumbersCheckBox.Location = new Point(601, 11);
+			capitaliseNumbersCheckBox.Location = new Point(600, 12);
 			capitaliseNumbersCheckBox.Name = "capitaliseNumbersCheckBox";
 			capitaliseNumbersCheckBox.Size = new Size(96, 25);
-			capitaliseNumbersCheckBox.TabIndex = 3;
+			capitaliseNumbersCheckBox.TabIndex = 4;
 			capitaliseNumbersCheckBox.Text = "Capitalise";
 			capitaliseNumbersCheckBox.UseVisualStyleBackColor = true;
-			capitaliseNumbersCheckBox.CheckedChanged += capitliseNumbersCheckBox_CheckedChanged;
+			capitaliseNumbersCheckBox.CheckedChanged += capitaliseNumbersCheckBox_CheckedChanged;
 			// 
-			// clearTranslationsButton
+			// allowMultilingualCheckbox
 			// 
-			clearTranslationsButton.Location = new Point(489, 11);
-			clearTranslationsButton.Margin = new Padding(4);
-			clearTranslationsButton.Name = "clearTranslationsButton";
-			clearTranslationsButton.Size = new Size(93, 50);
-			clearTranslationsButton.TabIndex = 2;
-			clearTranslationsButton.Text = "Clear";
-			clearTranslationsButton.UseVisualStyleBackColor = true;
-			clearTranslationsButton.Click += clearTranslationsButton_Click;
+			allowMultilingualCheckbox.AutoSize = true;
+			allowMultilingualCheckbox.Location = new Point(684, 4);
+			allowMultilingualCheckbox.Name = "allowMultilingualCheckbox";
+			allowMultilingualCheckbox.Size = new Size(208, 25);
+			allowMultilingualCheckbox.TabIndex = 9;
+			allowMultilingualCheckbox.Text = "Allow Multiple Languages";
+			allowMultilingualCheckbox.UseVisualStyleBackColor = true;
+			allowMultilingualCheckbox.CheckedChanged += Checkboxes_CheckedChanged;
 			// 
-			// translateButton
+			// languagesCombobox
 			// 
-			translateButton.Location = new Point(357, 11);
-			translateButton.Margin = new Padding(4);
-			translateButton.Name = "translateButton";
-			translateButton.Size = new Size(123, 50);
-			translateButton.TabIndex = 1;
-			translateButton.Text = "Translate";
-			translateButton.UseVisualStyleBackColor = true;
-			translateButton.Click += translateButton_Click;
+			languagesCombobox.FormattingEnabled = true;
+			languagesCombobox.Location = new Point(684, 41);
+			languagesCombobox.Name = "languagesCombobox";
+			languagesCombobox.Size = new Size(320, 29);
+			languagesCombobox.TabIndex = 8;
 			// 
-			// valueTextBox
+			// maximumValueTextbox
 			// 
-			valueTextBox.Location = new Point(10, 22);
-			valueTextBox.Margin = new Padding(4);
-			valueTextBox.Name = "valueTextBox";
-			valueTextBox.PlaceholderText = "Enter value to translate";
-			valueTextBox.Size = new Size(328, 29);
-			valueTextBox.TabIndex = 0;
+			maximumValueTextbox.Location = new Point(433, 40);
+			maximumValueTextbox.Name = "maximumValueTextbox";
+			maximumValueTextbox.Size = new Size(168, 29);
+			maximumValueTextbox.TabIndex = 7;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(302, 43);
+			label3.Name = "label3";
+			label3.Size = new Size(125, 21);
+			label3.TabIndex = 6;
+			label3.Text = "Maximum Value:";
+			// 
+			// minimumValueTextbox
+			// 
+			minimumValueTextbox.Location = new Point(128, 40);
+			minimumValueTextbox.Name = "minimumValueTextbox";
+			minimumValueTextbox.Size = new Size(168, 29);
+			minimumValueTextbox.TabIndex = 5;
+			minimumValueTextbox.KeyPress += textBox1_KeyPress;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(4, 43);
+			label2.Name = "label2";
+			label2.Size = new Size(123, 21);
+			label2.TabIndex = 4;
+			label2.Text = "Minimum Value:";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(379, 5);
+			label1.Name = "label1";
+			label1.Size = new Size(136, 21);
+			label1.TabIndex = 3;
+			label1.Text = "Number of places:";
+			// 
+			// decimalPlacesUpDown
+			// 
+			decimalPlacesUpDown.Location = new Point(521, 4);
+			decimalPlacesUpDown.Maximum = new decimal(new int[] { 5, 0, 0, 0 });
+			decimalPlacesUpDown.Name = "decimalPlacesUpDown";
+			decimalPlacesUpDown.Size = new Size(80, 29);
+			decimalPlacesUpDown.TabIndex = 2;
+			// 
+			// includeDecimalPlacesCheckbox
+			// 
+			includeDecimalPlacesCheckbox.AutoSize = true;
+			includeDecimalPlacesCheckbox.Location = new Point(187, 4);
+			includeDecimalPlacesCheckbox.Name = "includeDecimalPlacesCheckbox";
+			includeDecimalPlacesCheckbox.Size = new Size(186, 25);
+			includeDecimalPlacesCheckbox.TabIndex = 1;
+			includeDecimalPlacesCheckbox.Text = "Include Decimal Places";
+			includeDecimalPlacesCheckbox.UseVisualStyleBackColor = true;
+			includeDecimalPlacesCheckbox.CheckedChanged += Checkboxes_CheckedChanged;
+			// 
+			// includeCurrencyCheckbox
+			// 
+			includeCurrencyCheckbox.AutoSize = true;
+			includeCurrencyCheckbox.Location = new Point(4, 4);
+			includeCurrencyCheckbox.Name = "includeCurrencyCheckbox";
+			includeCurrencyCheckbox.Size = new Size(146, 25);
+			includeCurrencyCheckbox.TabIndex = 0;
+			includeCurrencyCheckbox.Text = "Include Currency";
+			includeCurrencyCheckbox.UseVisualStyleBackColor = true;
 			// 
 			// NumberTranslatorForm
 			// 
-			AcceptButton = translateButton;
 			AutoScaleDimensions = new SizeF(9F, 21F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(1008, 785);
@@ -133,15 +282,23 @@
 			Font = new Font("Segoe UI", 12F);
 			Margin = new Padding(4);
 			MinimizeBox = false;
+			MinimumSize = new Size(1024, 824);
 			Name = "NumberTranslatorForm";
 			Text = "Number Translator";
 			Activated += NumberTranslatorForm_Activated;
+			Shown += NumberTranslatorForm_Shown;
 			splitContainer1.Panel1.ResumeLayout(false);
 			splitContainer1.Panel1.PerformLayout();
 			splitContainer1.Panel2.ResumeLayout(false);
-			splitContainer1.Panel2.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
 			splitContainer1.ResumeLayout(false);
+			splitContainer2.Panel1.ResumeLayout(false);
+			splitContainer2.Panel1.PerformLayout();
+			splitContainer2.Panel2.ResumeLayout(false);
+			splitContainer2.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+			splitContainer2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)decimalPlacesUpDown).EndInit();
 			ResumeLayout(false);
 		}
 
@@ -149,9 +306,21 @@
 
 		private SplitContainer splitContainer1;
 		private TextBox translationTextBox;
-		private Button translateButton;
+		private SplitContainer splitContainer2;
 		private TextBox valueTextBox;
+		private Button translateButton;
 		private Button clearTranslationsButton;
 		private CheckBox capitaliseNumbersCheckBox;
+		private Button settingsToggleButton;
+		private CheckBox includeDecimalPlacesCheckbox;
+		private CheckBox includeCurrencyCheckbox;
+		private Label label2;
+		private Label label1;
+		private NumericUpDown decimalPlacesUpDown;
+		private TextBox minimumValueTextbox;
+		private TextBox maximumValueTextbox;
+		private Label label3;
+		private CheckBox allowMultilingualCheckbox;
+		private ComboBox languagesCombobox;
 	}
 }
--- NumberTranslator.WinForms/NumberTranslatorForm.cs
+++ NumberTranslator.WinForms/NumberTranslatorForm.cs
@@ -1,3 +1,5 @@
+using NumberTranslator.Interfaces;
+
 namespace NumberTranslator.WinForms
 {
 	public partial class NumberTranslatorForm : Form
@@ -16,12 +18,58 @@ namespace NumberTranslator.WinForms
 			valueTextBox.Focus();
 		}
 
+		private void SetSettingsButtonText(bool state)
+		{
+			settingsToggleButton.Text = state
+				? "Advanced Settings"
+				: "Simple Settings";
+		}
+
+		private void SetControlStates()
+		{
+			//	Decimal place changes are only allowed if the translation settings allow them
+			decimalPlacesUpDown.Enabled = includeDecimalPlacesCheckbox.Checked;
+
+			//	Alternate languages are only used in translations if settings permit
+			languagesCombobox.Enabled = allowMultilingualCheckbox.Checked;
+			if (!languagesCombobox.Enabled)
+				languagesCombobox.SelectedIndex = -1;
+
+			SetSettingsButtonText(splitContainer2.Panel2Collapsed);
+		}
+
+		private void NumberTranslatorForm_Shown(object sender, EventArgs e)
+		{
+			languagesCombobox.Items.AddRange(_factory.TranslatorLanguages.Values.Cast<object>().ToArray());
+			splitContainer2.Panel2Collapsed = true;
+			SetControlStates();
+		}
+
 		private void translateButton_Click(object sender, EventArgs e)
 		{
-			var translator = _factory.GetNumberTranslator("en", useTitleCase: capitaliseNumbersCheckBox.Checked);
 			var input = valueTextBox.Text.Trim();
 			try
 			{
+				INumberTranslator translator;
+				if (splitContainer2.Panel2Collapsed)
+				{
+					translator = _factory.GetNumberTranslator("en", useTitleCase: capitaliseNumbersCheckBox.Checked);
+				}
+				else
+				{
+					if (!double.TryParse(minimumValueTextbox.Text, out var minValue))
+						minValue = 0.0d;
+					if (!double.TryParse(maximumValueTextbox.Text, out var maxValue))
+						maxValue = 9999.0d;
+
+					//	Get the selected language, or use English as default if no selection made
+					var languageCode = languagesCombobox.SelectedIndex != -1
+						? _factory.TranslatorLanguages.First(kvp => kvp.Value == languagesCombobox.Text).Key
+						: "en";
+					translator = _factory.GetNumberTranslator(languageCode, includeCurrencyCheckbox.Checked,
+						includeDecimalPlacesCheckbox.Checked, (int)decimalPlacesUpDown.Value, minValue, maxValue,
+						capitaliseNumbersCheckBox.Checked);
+				}
 				var response = translator.Translate(input);
 				translationTextBox.AppendText($"'{input}' translates to {response}" + Environment.NewLine);
 			}
@@ -44,9 +92,38 @@ namespace NumberTranslator.WinForms
 			SetDefaultFocus();
 		}
 
-		private void capitliseNumbersCheckBox_CheckedChanged(object sender, EventArgs e)
+		private void capitaliseNumbersCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
 			SetDefaultFocus();
+		}
+
+		private void settingsToggleButton_Click(object sender, EventArgs e)
+		{
+			splitContainer2.Panel2Collapsed = !splitContainer2.Panel2Collapsed;
+			SetSettingsButtonText(splitContainer2.Panel2Collapsed);
+		}
+
+		private static char[] allowedChars = ['\b', '\t', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-'];
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//	Do not permit non-numeric characters, except backspace, tab, point and minus
+			var handled = !allowedChars.Contains(e.KeyChar);
+
+			//	Only perform additional checks if not already flagged as an ignored character, or the sender is not a TextBox
+			if (!handled && sender is TextBox textBox)
+			{
+				//	minus character must be first character
+				handled = (textBox.Text.Length > 0 && e.KeyChar == '-') ||
+									//	If decimals are permitted, make sure there's only one decimal point character!!
+									(e.KeyChar == '.' && (!includeDecimalPlacesCheckbox.Checked || textBox.Text.Contains('.')));
+			}
+			e.Handled = handled;
+		}
+
+		private void Checkboxes_CheckedChanged(object sender, EventArgs e)
+		{
+			SetControlStates();
 		}
 	}
 }
